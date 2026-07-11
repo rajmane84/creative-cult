05392d682f0a4b3929016ded334ebeb475362ea1
Raj Mane <rajmane5884@gmail.com>
2026-07-11 19:10:26 +0530

    complete setup of better auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello via Bun!');
+import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './auth';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
